c047a3fb7eddb8e0e911e892f47387c8d01b4810
Noiri <suleipenn@gmail.com>
2021-07-11 15:32:26 +0900

    募集をProjectCardコンポーネントに切り出し.

--- src/ProjectList/ProjectListPage.tsx
+++ src/ProjectList/ProjectListPage.tsx
@@ -1,10 +1,6 @@
 import { FC, useEffect, useState, useRef, useCallback } from "react";
-import Card from "@material-ui/core/Card";
-import CardMedia from "@material-ui/core/CardMedia";
 import { withStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
-import CardContent from "@material-ui/core/CardContent";
-import { Link } from "react-router-dom";
 import SearchIcon from "@material-ui/icons/Search";
 import Button from "@material-ui/core/Button";
 import { grey } from "@material-ui/core/colors";
@@ -13,6 +9,7 @@ import Toolbar from "@material-ui/core/Toolbar";
 import { Project } from "../DataType/ProjectType";
 
 import useStyles from "../CSS/ProjectListPageCSS";
+import ProjectCard from "./ProjectCard";
 
 type Response = {
   data: {
@@ -118,26 +115,8 @@ const ProjectListPage: FC = () => {
       </AppBar>
       <div className={classes.projectListOuterLayout}>
         <p>{notFound ? "お探しの募集は見つかりませんでした." : ""}</p>
-
         {projectList !== undefined
-          ? projectList.map((x) => (
-              <Link style={{ textDecoration: "none" }} to={`/projects/${x.id}`}>
-                <Card key={x.id} className={classes.projectCardStyle}>
-                  <CardMedia
-                    className={classes.projectImage}
-                    image={x.imageUrlSmall}
-                    title={x.title}
-                  />
-                  <div>
-                    <CardContent className={classes.projectTitleLayout}>
-                      <Typography component="h5" variant="h5">
-                        {x.title}
-                      </Typography>
-                    </CardContent>
-                  </div>
-                </Card>
-              </Link>
-            ))
+          ? projectList.map((project) => <ProjectCard project={project} />)
           : "undefined."}
       </div>
     </>
