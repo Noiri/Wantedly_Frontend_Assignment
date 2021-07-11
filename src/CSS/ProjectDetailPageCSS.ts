7a2ea66c142fd4f54112b2eb91497d749311c3da
Noiri <suleipenn@gmail.com>
2021-07-11 17:20:32 +0900

    HeadderのCSSを全てHeadderCSSに移動.

--- src/CSS/ProjectDetailPageCSS.ts
+++ src/CSS/ProjectDetailPageCSS.ts
@@ -1,13 +1,6 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
-  appbarTitle: {
-    flexGrow: 1,
-  },
-  headerStyle: {
-    backgroundColor: "#313F5C",
-    flexGrow: 1,
-  },
   projectTtile: {
     marginLeft: "10%",
   },
